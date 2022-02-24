bc4419ed77d97546244ee32203ff607b19b9a183
jaem1n207 <roy.jm.lee@gmail.com>
2022-02-24 12:57:12 +0900

    feat: setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,7 @@ export default function Home() {
   return (
     <main>
       <Title>Hello TypeScript!</Title>
-      <p>A TypeScript starter for Gatsby. Great for advanced users.</p>
-      <p>
-        Follow me on Twitter (
-        <a href="https://twitter.com/jpedroschmitz">@jpedroschmitz</a>)
-      </p>
+      Hello!
     </main>
   );
 }
